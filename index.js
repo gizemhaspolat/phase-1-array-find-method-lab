6346f4d9f95bd5a1f1569fc00dedb77dca838f23
Gizem Haspolat <gizemhaspolat@gmail.com>
2022-04-22 21:48:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(record) {
+  let bowl = 0;
+  if ((bowl = record.find((element) => element.result === "W"))) {
+    return bowl.year;
+  } else if ((bowl = record.find((element) => element.result === "N/A"))) {
+    return undefined;
+  }
+}
+
+console.log(superbowlWin(record));
